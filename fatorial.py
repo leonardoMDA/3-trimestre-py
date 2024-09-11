77bf06d2b092dbe42357d74147a1b03a7642caaf
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-09-11 11:51:37 -0300

    Add files via upload

---
+++ fatorial.py
@@ -0,0 +1,12 @@
+num = int(input("Digite um número: "))
+
+fatorial = 1
+
+if num < 0:
+    print("Não é possível calcular o fatorial de um número negativo.")
+elif num == 0:
+    print("O fatorial de 0 é 1.")
+else:
+    for i in range(1, num + 1):
+        fatorial *= i
+    print(f"O fatorial de {num} é {fatorial}.")
